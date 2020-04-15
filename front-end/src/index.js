86787ca597183b4452c6e44672bdaff22622de46
Rostislav Georgiev <rostislav.g.georgiev@gmail.com>
2020-04-15 19:53:53 +0300

    Front end setup

---
+++ front-end/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Fintooth = () => {
+    return <h1>Welcome to fintooth!</h1>
+}
+
+ReactDOM.render(<Fintooth />, document.getElementById('root'))
